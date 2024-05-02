74129caf9d642675debd88de20d4b10a37e03f5f
nelsonokothguya <nelsonokothguya15@gmail.com>
2024-05-03 00:42:31 +0300

    flex layout for dashboard apply

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,29 +13,33 @@ export default function Page() {
       <header className="flex justify-between">
         <Header />
       </header>
-      <div className="w-full h-full flex">
-        <div className="w-1/6 border-r bg-gray-100 border-gray-300">
+      <div className="flex w-full h-full">
+        <div className="w-1/6 h-full bg-gray-100 border-r border-gray-300">
           <Sidenav />
         </div>
-        <div className="w-1/2 h-full mt-5 border-r bg-white border-gray-300">
-          <div className="p-6 font-bold ">
+        <div className="flex flex-col w-1/2 h-full mt-5 bg-white border-r border-gray-300">
+          <div className="p-6 font-bold">
             <h1>Dashboard</h1>
           </div>
-          <div className="h-1/2 border-b border-gray-300">
-            <RevenueChart />
-          </div>
-          <div className="flex w-full h-full">
-            <RatingsChart />
-            <div className="w-1/2 border-l border-gray-300">
-              <MostOrderedFoodsList />
+          <div className="flex flex-col flex-grow">
+            <div className="flex-grow h-1/2 border-b border-gray-300">
+              <RevenueChart />
+            </div>
+            <div className="flex w-full h-1/2">
+              <div className="w-1/2">
+                <RatingsChart />
+              </div>
+              <div className="w-1/2 border-l border-gray-300">
+                <MostOrderedFoodsList />
+              </div>
             </div>
           </div>
         </div>
-        <div className="w-1/4 mt-24 bg-white">
-          <div>
+        <div className="w-1/4 h-full mt-24 bg-white">
+          <div className="h-1/2">
             <OrderTimeChart />
           </div>
-          <div className="border-t border-gray-300">
+          <div className="h-1/2 border-t border-gray-300">
             <OrderTrendChart />
           </div>
         </div>
